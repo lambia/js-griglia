aef528240504ca19461a2f8d7a4dcd3ae970a86c
Luca Lambiase <luca.lambia@gmail.com>
2023-11-08 13:43:24 +0100

    lotteria

--- js/main.js
+++ js/main.js
@@ -1,39 +1,84 @@
-// <div class="square"></div>
+// al caricamento della pagina
+// creaGriglia();
 
-//creare la griglia dinamicamente, inserendo N elementi .square nel container
-let numCelle = 64;
-let grid = document.getElementById("grid");
-
-for (let i = 1; i <= numCelle; i++) {
-    
-    let cella = creaQuadrato(i);
-    grid.appendChild(cella);
-
-}
+document.getElementById("start").addEventListener("click", function () {
+    console.log("inizio partita");
+    creaGriglia();
+});
 
 /******************************************************************** */
+//creare la griglia dinamicamente, inserendo N elementi .square nel container
+function creaGriglia() {
+    const grid = document.getElementById("grid");
+    //ci assicuriamo che la griglia sia vuota
+    grid.innerHTML = "";
 
-function creaQuadrato(numero) {
+    //recupera il livello in base al valore della select
+    const livello = getLivello();
+    let numCelleTotali;
+    let numCellePerRiga;
+
+    if (livello == 1) {
+        numCelleTotali = 81;
+        numCellePerRiga = 9;
+    } else if (livello == 2) {
+        numCelleTotali = 64;
+        numCellePerRiga = 8;
+    } else if (livello == 3) {
+        numCelleTotali = 49;
+        numCellePerRiga = 7;
+    }
+
+    const cellaFortunata = getRndInteger(1, numCelleTotali);
+    document.getElementById("msg").innerText = `Cella fortunata: ${cellaFortunata}`;
+
+    for (let i = 1; i <= numCelleTotali; i++) {
+
+        let cella = creaQuadrato(i, cellaFortunata);
+        cella.style.width = `calc(100% / ${numCellePerRiga})`;
+        cella.style.height = `calc(100% / ${numCellePerRiga})`;
+
+        if(i==cellaFortunata) {
+            cella.style.border = "solid 1px red";
+        }
+
+        grid.appendChild(cella);
+
+    }
+}
+
+function getLivello() {
+
+    const livello = parseInt(document.getElementById("level").value);
+    console.log("Difficoltà: ", livello);
+    return livello;
+}
+
+function creaQuadrato(numero, cellaFortunata) {
 
     const cella = document.createElement("div");
     cella.classList.add("square");
+    cella.innerText = numero;
 
     //per ogni quadrato voglio un evento che gestisca il click
-    cella.addEventListener("click", function() {
+    cella.addEventListener("click", function () {
 
-        console.log("Cella cliccata", numero);
-        
-        //versione abbreviata
+        if(numero==cellaFortunata) {
+            console.log("Hai vinto", numero);
+        } else {
+            console.log("Sbagliato, ritenta", numero);
+        }
+
+        // let numeroCella = parseInt(cella.innerText);
+        // console.log("Cella cliccata (inner)", numeroCella);
+
         cella.classList.toggle("highlight");
-
-        //versione completa
-        // if(cella.classList.contains("highlight")) {
-        //     cella.classList.remove("highlight");
-        // } else {
-        //     cella.classList.add("highlight");
-        // }
 
     });
 
     return cella;
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
